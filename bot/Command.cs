c9fa6bad1d6e613cf0be34b26325e1cdc0de8e40
Yotsuhi <toho_momiji_104@yahoo.co.jp>
2020-12-20 00:11:34 +0900

    Revert "revert"
    This reverts commit a571b2c19e818b13d8f76c6d4567a1bf3e75722c.

---
+++ bot/Command.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JinroGM.bot
+{
+    class Command
+    {
+
+    }
+}
---
+++ utility/ConstValue.cs
@@ -0,0 +1,13 @@
+﻿namespace JinroGM
+{
+    static class ConstValue
+    {
+        // ディレクトリパス
+        public const string ROOT_PATH = "../../../";                              // ルートディレクトリのパス
+        public const string DATAS_DIRECTORY_PATH = "datas/";          // datasディレクトリのパス
+
+
+        // ファイルパス
+        public const string TOKEN_FILE_PATH = "token.txt";      // トークン記号を記録したファイルのパス
+    }
+}
